92da1dcd242b86d1543c1c049f0d96148aaee35a
Zhe Zheng <zhez@supermicro.com>
2017-06-30 18:13:07 -0700

    LC19_RemoveNthNodeFromEndofList

--- LinkedList/LC2_AddTwoNumbers.cpp
+++ LinkedList/LC2_AddTwoNumbers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode res(0), *p = &res;
+        ListNode res(0), *p = &res;                 //Initialize ListNode res(0)
         int carry = 0;
         while(l1 || l2 || carry){
             carry += (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
@@ -12,5 +12,31 @@ public:
             l2 = l2 ? l2->next : l2;
         }
         return res.next;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head=NULL, p=NULL;
+        int carry=0;
+        while(l1 || l2){
+            if(l1) carry += l1->val;
+            if(l2) carry += l2->val;
+            if(!head){
+                head = new ListNode(carry%10);      //Initialize head = new ListNode(carry%10)
+                p = head;
+            }
+            else{
+                p->next = new ListNode(carry%10);
+                p = p->next;
+            }
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+            carry /= 10;
+        }
+        if(carry>0) p->next = new ListNode(carry);
+        return head;
     }
 };
